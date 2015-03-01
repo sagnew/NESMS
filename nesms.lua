efc93c9a4eb1de59a925dbfb4ce6740bb09f2543
Sam Agnew <sagnew@eden.rutgers.edu>
2015-03-01 08:09:46 -0500

    Add infrastructure for arbitrary code entry via sms

--- nesms.lua
+++ nesms.lua
@@ -1,3 +1,4 @@
+prev = "";
 msg = "";
 
 function write_file (filename, text)
@@ -16,12 +17,25 @@ function read_file (filename)
     return input_content;
 end;
 
+function handle_input ()
+    local address = tonumber(read_file("address.txt"), 16);
+    local value = tonumber(read_file("value.txt"), 16);
+    if(address ~= "None" and address ~= nil and value ~= nil) then
+        memory.writebyte(address, value);
+    end;
+end;
+
 while (true) do
     input_content = read_file("input.txt");
 
     if(input_content ~= nil) then
         msg = input_content
         gui.text(0, 50, msg);
+    end;
+
+    if(msg ~= prev) then
+        prev = msg;
+    	handle_input()
     end;
 
     emu.frameadvance();
