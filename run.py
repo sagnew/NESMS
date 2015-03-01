85ee529035df1155eaf1a3bed350b461e93567eb
Manuel Lopez <manny.lopez625@gmail.com>
2015-03-01 06:35:12 -0500

    disabled resonse for testing. file writing done

--- run.py
+++ run.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    resp = twilio.twiml.Response()
-    resp.message("yo homie yo")
+    #resp = twilio.twiml.Response()
+    #resp.message("yo homie im playing nes")
+
     print request.form['Body']
     print request.form['From']
-    return str(resp)
+
+    with open("msgs.txt", "a+") as f:
+        f.write(request.form['From'] + ":" + request.form['Body']+"\n")
+    #return str(resp)
+    return 'recieved'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
