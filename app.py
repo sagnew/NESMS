26c77bb6ed7946fffe22546cbad3636d167e7760
Sam Agnew <sagnew@eden.rutgers.edu>
2015-08-03 00:36:59 -0400

    Update code to match the blog post tutorial

---
+++ app.py
@@ -0,0 +1,53 @@
+import re
+
+from flask import Flask, request
+from twilio.twiml import Response
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def sms_reply():
+    # Retrieve the body of the text message.
+    message_body = request.form['Body']
+    print message_body
+
+    # Create a TwiML response object to respond to the text message.
+    resp = Response()
+    message_response = 'Message received! Manipulating memory now.'
+    error_message = ('Please enter a 4 digit hex address and a 2 digit '
+                     'hex value separated by a space. '
+                     'For example: "066D FF"')
+
+    # Create a list of all words in the message body.
+    message_list = message_body.split(' ')
+
+    # Create a regex for matching hex Strings.
+    hex_pattern = re.compile('^[0-9a-fA-F]+$')
+
+    # Make sure the message is in the right format.
+    if not len(message_list) == 2:
+        message_response = error_message
+    else:
+        # The first word should be the hex address.
+        address = message_list[0]
+
+        # The second word should be the hex value to write to the address.
+        value = message_list[1]
+
+        # Check to see if the address and value are valid hexadecimal numbers.
+        if hex_pattern.match(address) and hex_pattern.match(value):
+            # Write the address and value to their respective text files.
+            with open('address.txt', 'w') as f:
+                f.write(address)
+
+            with open('value.txt', 'w') as f:
+                f.write(value)
+        else:
+            message_response = error_message
+
+    resp.message(message_response)
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- nesms.lua
+++ nesms.lua
@@ -1,48 +1,38 @@
-prev = "";
-msg = "";
-
-function write_file (filename, text)
-    output = io.open(filename, "w");
-    io.output(output);
-    io.write(text);
-    io.close(output);
-end;
-
+-- A function to read text files
 function read_file (filename)
-    input = io.open(filename, "r");
-    io.input(input);
-    input_content = io.read();
-    io.close(input);
+    input = io.open(filename, "r") -- Open this file with the read flag.
+    if input ~= nil then
+      io.input(input) -- Set the input that the io library will read from.
+      input_content = io.read() -- Read the contents of the file.
+      io.close(input) -- Close the file.
+    end
 
-    return input_content;
-end;
+    return input_content
+end
 
-function handle_input ()
-    local address = tonumber(read_file("address.txt"), 16);
-    local value = tonumber(read_file("value.txt"), 16);
-    local speed = read_file("speed.txt");
+prev_address = '' -- Variable to keep track of the address.
+prev_value = '' -- Variable to keep track of the value.
 
-    if(address ~= "None" and address ~= nil and value ~= nil) then
-        memory.writebyte(address, value);
-    end;
+-- Infinite loop to take control over frame advancing.
+while true do
+  address = read_file('address.txt')
+  value = read_file('value.txt')
 
-    if(speed ~= "None") then
-    	emu.speedmode(speed);
-    end;
-end;
+  -- Only write to memory if the value has changed.
+  if address ~= prev_address or value ~= prev_value then
+    hex_address = tonumber(address, 16)
+    hex_value = tonumber(address, 16)
 
-while (true) do
-    input_content = read_file("input.txt");
+    -- Check to see if the entered strings are valid.
+    if hex_address == nil or hex_value == nil then
+      emu.message('Invalid address or value. Please use valid hex numbers.')
+    else
+      memory.writebyte(hex_address, hex_value) -- Base 16 for hex values
+      emu.message(address..': '..value) -- Print address and value being changed.
+    end
+  end
 
-    if(input_content ~= nil) then
-        msg = input_content
-        emu.message(msg);
-    end;
-
-    if(msg ~= prev) then
-        prev = msg;
-    	handle_input()
-    end;
-
-    emu.frameadvance();
-end;
+  prev_address = address -- Update the address to keep track of changes.
+  prev_value = value -- Update the value to keep track of changes.
+  emu.frameadvance() -- This essentially tells FCEUX to keep running.
+end
